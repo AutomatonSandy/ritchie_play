fe319e875ff613387e926f64bf392b9d19c22726
Sandipan Bhattacharjee <sandipanbhattacharjee1981@gmail.com>
2026-03-17 14:36:04 -0700

    fixing up github actions names, test specific logs

--- tests/ritchie_ford.spec.ts
+++ tests/ritchie_ford.spec.ts
@@ -3,16 +3,17 @@ import {PageObjectManager} from '../pages/PageObjectManager';
 
 
 
-test('ritchie ford related tests @ford @smoke',async({page})=>{  
+test('ritchie ford related tests @ford @smoke',async({page}, testInfo)=>{  
    await page.screenshot({path: 'test-results/before-test.png'});
+   const prefix = `[${testInfo.project.name.concat('>').concat(testInfo.titlePath.join(' > '))}]`;
    const searchTerm = 'Ford F-150'; 
    const pageManager = new PageObjectManager(page);
    await pageManager.getTopPanel().searchOnTopPanel(searchTerm);
    const numberOfResults = await pageManager.getSearchResults().extractNumberOfResults();
-   console.log(`Number of results extracted: ${numberOfResults}`);
+   console.log(`${prefix} Number of results extracted: ${numberOfResults}`);
    await expect(numberOfResults).toBeGreaterThan(0);
    const firstResultTitle = await pageManager.getSearchResults().returnTheFirstResultTitle();
-   console.log(`First result title extracted: ${firstResultTitle}`);
+   console.log(`${prefix} First result title extracted: ${firstResultTitle}`);
    await expect(firstResultTitle).toContain(searchTerm);
 
 })
