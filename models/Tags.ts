2eb4f28796e0f5f37f8a0e958ce415f9546c7417
Sandipan Bhattacharjee <sandipanbhattacharjee1981@gmail.com>
2026-03-21 23:04:45 -0700

    Adding new api changes and tests

---
+++ models/Tags.ts
@@ -0,0 +1,25 @@
+export class Tags {
+  private _id: number;
+  private _name: string;
+
+  constructor();
+  constructor(id: number, name: string);
+  constructor(id?: number, name?: string) {
+    this._id = id ?? 0;
+    this._name = name ?? "";
+  }
+
+  get id(): number {
+    return this._id;
+  }
+  set id(value: number) {
+    this._id = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  set name(value: string) {
+    this._name = value;
+  }
+}
